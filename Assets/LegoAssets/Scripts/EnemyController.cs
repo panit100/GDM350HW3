2ea7af35289115188767023d81cf013f36b909eb
Panit <panit.cuteza@mail.kmutt.ac.th>
2021-02-08 08:49:11 +0700

    Initial commit

---
+++ Assets/LegoAssets/Scripts/EnemyController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour {
+
+	public GameObject Gameover;
+	public Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+	public float delayEnemy = 3;
+	public bool isWalking = false;
+	
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+
+	void Start() {
+		StartCoroutine(DelayEnemy());
+	}
+	
+	IEnumerator DelayEnemy() {
+		yield return new WaitForSeconds(delayEnemy);
+		isWalking = true;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (isWalking) {
+			nav.SetDestination (player.position);
+		} else {
+			nav.SetDestination (gameObject.transform.position);
+		}
+	}
+
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.tag == "Player") {
+
+			GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+			
+			foreach (GameObject enemy in enemys) {
+				enemy.SendMessage("PlayerDie");
+			}
+
+			col.gameObject.GetComponent<PlayerMovement>().enabled = false;
+			Gameover.SetActive(true);
+			StartCoroutine("RestartGame");
+		}
+	}
+
+	
+
+	IEnumerator RestartGame(){
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(0);
+	}
+}
